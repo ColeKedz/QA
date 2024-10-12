aa7d0b01dd639fca10ec47e2e05f5d68aaa017cf
ColeKedz <colerkedz@gmail.com>
2024-10-12 16:47:34 -0400

    restAssured added and simple api script created

---
+++ PracticeTestNG/src/test/java/restAssured/CatsFreeAPI.java
@@ -0,0 +1,26 @@
+package restAssured;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import io.restassured.RestAssured;
+import io.restassured.response.ResponseBody;
+import static io.restassured.RestAssured.given;
+
+public class CatsFreeAPI {
+
+		@Test
+		public void verify200Status() {
+			
+			RestAssured.baseURI = "https://catfact.ninja";
+			
+			int statusCode = given().when().get("https://catfact.ninja/breeds?limit=1").getStatusCode();
+			
+			ResponseBody<?> response = given().when().get("https://catfact.ninja/breeds?limit=1").getBody();
+			
+			Assert.assertEquals(200, statusCode);
+
+			Reporter.log(response.asPrettyString());
+			
+		}
+	
+}
