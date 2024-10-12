f9189d0f2f58fef0ef2c065ca0dc858a1b6e2092
ColeKedz <colerkedz@gmail.com>
2024-10-12 15:52:38 -0400

    replaced system out with reporting for better test result emailable file

--- PracticeTestNG/src/test/java/Google/searching.java
+++ PracticeTestNG/src/test/java/Google/searching.java
@@ -1,6 +1,7 @@
 package Google;
 
 import org.testng.Assert;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -12,8 +13,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.testng.asserts.*;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
@@ -40,21 +39,23 @@ public class searching {
 	public void search() throws InterruptedException {
 		driver.get("https://google.com");
 		driver.findElement(By.name("q")).sendKeys("cole kedzierski", Keys.ENTER);
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 	
 		
 		List<WebElement> searchResults =  driver.findElements(By.className("yuRUbf"));
-		System.out.print(searchResults.getFirst().toString());
+		Reporter.log("the first web element collected was: " + searchResults.getFirst().getText());
 		searchResults.getFirst().click();
-		Thread.sleep(5000);
+		Thread.sleep(3000);
+		
 		
 		String expectedPage = "https://www.linkedin.com/in/cole-kedzierski";
 		String currentPage = driver.getCurrentUrl();
-		System.out.print(currentPage);
+		Reporter.log("The page that the test ended up on was: " + currentPage);
+		
+		
 		Assert.assertEquals(currentPage, expectedPage);
+	
 		
-		
-		driver.quit();
 	}
 	
 	
